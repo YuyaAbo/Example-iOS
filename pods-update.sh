9aece193eac32b288d109c67e73bf6b1f9885cc0
YuyaAbo <b1011211@gmail.com>
2020-03-01 21:48:55 +0900

    a

--- pods-update.sh
+++ pods-update.sh
@@ -6,10 +6,9 @@ pod update
 
 if [[ `git status --porcelain --untracked-files=no` ]]; 
 then
-  # git commit and push
+  # git commit
   git add Podfile.lock
   git commit -m 'Update CocoaPods dependences'
-  git push origin HEAD
 else
   echo "No changes."
 fi
