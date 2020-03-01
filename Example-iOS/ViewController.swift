5837b5b3d7f932967a38ea067b3b645baf8f6624
YuyaAbo <b1011211@gmail.com>
2020-03-01 15:51:33 +0900

    first

---
+++ Example-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example-iOS
+//
+//  Created by YuyaAbo on 2020/03/01.
+//  Copyright © 2020 YuyaAbo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,7 @@
+# platform :ios, '9.0'
+
+target 'Example-iOS' do
+  use_frameworks!
+
+  pod 'Alamofire', '~> 5.0'
+end
